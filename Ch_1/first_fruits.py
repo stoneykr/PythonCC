20ca8adde7b74315eeed093cb626cbe265737e18
Kyle Stoney <146282891+stoneykr@users.noreply.github.com>
2025-04-12 08:23:45 -0700

    Delete Ch_1 directory
    Wanted to ignore this but since i pushed it once i cannot get it to stop uploading

--- Ch_1/first_fruits.py
+++
@@ -1,9 +0,0 @@
-print("Jesus is Lord!")
-print("Jesus is King!")
-print("Jesus is the Way!")
-print("Hello world!")
-
-# This should be ignored.
-print ("This is a test to see if it gets ignored.")
-
-#Wheel addd one more to see whats what.
