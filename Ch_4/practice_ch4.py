4726e1d9cc26f7d19e4f63211551908f423c8569
Kyle Stoney <stoneykr@gmail.com>
2025-04-13 17:01:23 -0700

    Ch4 practice and quizzes

--- Ch_4/practice_ch4.py
+++ Ch_4/practice_ch4.py
@@ -1,40 +1,83 @@
+magicians = ["Houdini", "Penn & teller", "david blaine", "david copperfield"]
+for magician in magicians:
+    print(f"\n\t{magician}")
+print (magicians)
+
+#sort the list then print again
+
+magicians.sort()#Not working. List maintains order
+for magician in magicians:
+    print (f"\n\t\t{magician.title()}")
+
+print (magicians)
+#IDK why .sort() doesn't work but .sort(reverse=True) does??? Oh well.
+
+for magician in magicians:
+    print(f"\t{magician.title()}, that was a great trick!")
+    print(f"\tI can't wait to see {magician.title()}'s next trick!\n")
+
+print ("Thanks all for playing!")
+
+
+#Numbers: Off by one
+
+for value in range (-2, 5):
+    print (value)
+    print(f"\t {value}")
+
+for value in range(5):
+    print(value)
+
+
+numbers = list(range(65, 70))
+#Two different types of ouputs:
+# 1:
+print(numbers)
+
+# 2:
+for number in numbers:
+    print (number)
+
+#number lists (first=start, second=end, third=multiple)
+evens = list(range(2, 22, 2)) 
+#If second is 20, output is 18. If 21 or 22, output is 20 *OFF BY 1!
+print (evens)
+thirds = list(range(3, 31, 3))
+print (thirds)
+for third in thirds:
+    print(f"\n\t{third}")
+
+#generating numbers into list
+squares = ['Empty Squares List']
+print(squares)
+del squares[0]
+for value in range(1, 11):
+   square = value **2
+   squares.append(square)
+   print(square)
+
+print(squares)
+
+
+#List comprehension does the same thing more efficiently
+cubes = [value **3 for value in range(1, 11)] #<--- list comprehension
+print(f"\t {cubes}")
+for cube in cubes:
+    print(f"\t{cube}")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Slicing a list
+players=["chahtles", 'mellisa', 'tandy', 'carol', 'erica', 'dingoatemybaby']
+print(players[4:])
+print(players[:3])
+print(players[1:6:2])
+print(players[:-2])
+print(players[-2:])
+for player in players[:-3]:
+    print(player.title())
+for player in players[-3:]:
+    print (player.upper())
 
 
 
--- Ch_4/quiz_ch4.py
+++ Ch_4/quiz_ch4.py
@@ -1,27 +1,49 @@
+# 4-1 Pizza...favorite pizza
+favorite_pizza = ['hawaiian', "bacon, pepperoni, and olive", 'sausage']
+for pizza in favorite_pizza:
+    print(f"I often order {pizza} pizza.")
+print('\tI really love pizza')
+
+# 4-2 Animals
+animals = ['cat', 'dog', 'chicken']
+for animal in animals:
+    print (f"A {animal} is an animal you'd find on a farm, typically.")
+print("\tI have been attacked by each of them at one point in time in my life.")
 
 
 
+#4-3 counting to 20
+twenty = [value +1 for value in range(0, 20)]
+print(twenty)
+for digit in twenty:
+    print(digit)
 
 
+#4-4 Million. Will block this off to keep file small
+million = range(0, 1000000)
+#for digit in million:
+    #print (digit)
 
 
+#4-5 summing million
+print (f'The minimum in a list from 0 to one million is {min(million)}')
+print (f"The maximum in a list from 0 to one million is {max(million)}")
+print (f"The SUM of numbers from 0 to one million is {sum(million)}!")
 
+# 4-6
+thirds = (range(1, 20, 3))
+for third in thirds:
+    print (third)
 
+# 4-7
+threes = [value*3 for value in range(1, 11)]
+for third in threes:
+    print(f"\t {third}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 4-8/9
+cubes =[value**3 for value in range(1, 11)]
+for cube in cubes:
+    print (cube)
 
 
 
