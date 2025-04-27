a17c4a0b5a1ce4efec92c9590826f304bf6db564
Kyle Stoney <stoneykr@gmail.com>
2025-04-26 19:07:13 -0700

    Couple of days of code

--- Ch_5/practice_ch5.py
+++ Ch_5/practice_ch5.py
@@ -1,34 +1,82 @@
+cars = ['ram', 'bmw', 'dodge', 'toyota', 'honda']
+
+cars.sort()
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    elif car== 'ram':
+        print("rAAAAm-a-lamma-ding-dong")
+    else:
+        print (car.title())
+
+
+toppings = ['mushrom', 'peperoni', 'sausage', 'bacon']
+for topping in toppings:
+    if topping != "anchovies":
+        print(f"Add {topping.title()}")
+
+if "anchovies" not in toppings:
+    print("\tHold the anchovies!!!")
+
+answer = 1
+if answer == 19:
+    print("Great job!")
+else:
+    print("What a loser! Try again!")
 
 
 
 
+banned_users = ['phillip', 'james', 'price']
+user = "marjorie"
+if user not in banned_users:
+    print(f"{user.title()}, you may enter the chat.")
 
 
 
 
+#age = input("Enter your age : ") I turned this off to test run without interruption.
+#age = int(age)
+age = 69
+if age < 4:
+    price = 0.00
+elif age < 19:
+    price = 18.00
+elif age <65:
+    price = 28.00
+elif age >= 65:
+   price = 0.00
+
+print (f"Your admission cost is ${price}. Have a great time!")
+
+
+pizza_toppings = []
+if pizza_toppings:
+    for topping in pizza_toppings:
+        print (f"Adding {topping.title()}")
+    print("End of Topping List")
+else:
+    print("Is you sure you want a plain pizza?")
 
 
 
+requested_toppings = ['bacon', 'ham', 'extra cheese', 'pineapple']
 
+available_toppings = ["peperoni", "jalapenos", "ham", "bacon", "extra cheese"]
 
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping.title()}")
+    else:
+        print(f"Sorry, we do not have {requested_topping.title()}.")
+print("Done making your crappy pizza!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
+Is this a comment, too?
+We'll see
+'''
+'''Yes it is a comment!'''
 
 
 
--- Ch_5/quiz_ch5.py
+++ Ch_5/quiz_ch5.py
@@ -1,99 +1,151 @@
 
+#5.1
+car = 'subaru'
+print("Is car == 'subaru'? I predict: True.")
+print (car=='subaru')
+
+sandwich = 'Ruben'
+print('Is sandwich == ruben? I say: True!')
+print (sandwich.lower() == 'ruben')
+
+
+sport = 'hockey'
+sports = ["hockey", "basketball", "golf"]
+if sport not in sports:
+    print(f"Sorry, but we don't consider {sport.upper()} a real sport.")
+else:
+    print(f"Congrats! We consider {sport.upper()} to be a real sport!")
+
+
+#5.3 Alien Colors 1
+alien_color = 'Green'
+if alien_color.lower() == 'green':
+    print("5 Points")
+
+alien_color = "Purple"
+if alien_color.lower() == 'yellow':
+    print ("60 Points")
+else:
+    print ("Ship ain't Purple, my dude.")
+
+
+# 5.4 Alien Colors 2:
+alien_color = "Green"
+if alien_color == "Green":
+    print ("5 Points!")
+elif alien_color != "Green":
+    print ("Wrong Ship, Playa!")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+alien_color = "Purple"
+if alien_color == "Green":
+    print ("5 Points!")
+elif alien_color != "Green":
+    print ("Wrong Ship, Playa!")
+
+
+#5.4 Alien Colors 3:
+alien_color = input ("Input alien color (Green, Yellow, Red): ")
+alien_color = alien_color.title()
+if alien_color == "Green":
+    print ("5 Points!")
+elif alien_color == "Yellow":
+    print("30 Points")
+elif alien_color == "Red":
+    print("100 Points")
+
+
+
+
+
+# 5.6 Stages of Life:
+age = input ("Enter your age: ")
+age = int(age)
+if age < 2:
+    print ("You are a baby.")
+elif age <4:
+    print ("You are a toddler")
+elif age <19:
+    print ("You are a Teenager")
+elif age <65:
+    print ("You are an adult")
+elif age >= 65:
+    print ("You are a senior. Congratulations. You made it. Much honor uppon you. (BOW)")
+
+
+#5-7 Favorite Fruit
+favorite_fruits = ['apple', 'pear', 'plumb']
+if 'apple' in favorite_fruits:
+    print ("Man, you really love apples.")
+if 'pear' in favorite_fruits:
+    print ("Boy! You sure do like pears!")
+if 'plumb' in favorite_fruits:
+    print ("Shoot! I din't know you loved dems plumbs!")
+
+#import random section not in book
+import random
+random_agent = random.randint (0,6)
+
+agents = ['gadget', 'oper', 'crane', 'cat', 'slither', 'fade', 'burrow']
+
+print(f"Your random agent is: {random_agent}!")
+print(f"For your next mission, you will work with agent {agents[random_agent].title()}!")
+
+print ("Your name is " + input("What is your name? "))
+
+# 5.8 Hello admin
+users = ['chatles', 'barbararah', 'fat tony', 'admin', 'arthulio']
+for user in users:
+    if user == "admin":
+        print(f"Good morning, {user.title()}! How would you like to proceed.")
+    else:
+        print(f"Hello {user.upper()}.")
+
+#5.8.1 Using randomg enerator to take a random admin to print a greeting (not in book)
+import random
+random_user = random.randint (0,4)
+users = ['chatles', 'barbararah', 'fat tony', 'admin', 'arthulio']
+print (f"Your random user is: {users[random_user]}")
+if users[random_user] == 'admin':
+    print(f"\n\tHello {users[random_user].upper()}! It's good to see you again!")
+else:
+    print(f"\n\tOh, {users[random_user].title()}. It's you again.")
+
+
+# 5.9
+new_users = []
+if new_users == []:
+    print ("We need new users")
+
+# 5.10 Checking User Names
+current_users = ['penguinkilla02', 'gb0mB5', 'Al_Chemie', 'Mur_curie', 'alphalPhapug99']
+new_users = ['Eyma5kamma', 'penguinkilla02', 'Al_Chemie', 'dopel_gangher','fartfignewton']
+current_users_lower = []
+
+for item in current_users:
+    current_users_lower.append(item.lower())
+
+print (current_users_lower)
+
+
+
+for new_user in new_users:
+    if new_user.lower() not in current_users_lower:
+        print(f"Welcome to the club {new_user}!")
+    else:
+        print (f"We're sorry, but {new_user} is already taken.")
+
+
+#5.11 Ordinal Numbers
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+for number in numbers:
+    if number == '1':
+        print(f"{number}st")
+    elif number == '2':
+        print(f"{number}nd")
+    elif number == '3':
+        print(f"{number}rd")
+    else:
+        print(f"{number}th")
 
 
